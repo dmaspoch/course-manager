e016ca1cf3664aeebe24bdc3033838435978d4dc
dmaspoch <dmaspoch@hotmail.com>
2024-08-07 11:29:06 -0400

    Added semi-working file upload

--- controllers/lectureController.js
+++ controllers/lectureController.js
@@ -1,57 +1,56 @@
 const Lecture = require("../models/lecture");
 const Course = require("../models/course");
 
-  exports.addLecture = (req, res, next) => {
-    res.render("./add-lecture", {
-      pageTitle: "Add Lecture",
-      editing: false,
-      courseId: req.params.courseId
-    });
-  };
+exports.addLecture = (req, res, next) => {
+  res.render("./add-lecture", {
+    pageTitle: "Add Lecture",
+    editing: false,
+    courseId: req.params.courseId
+  });
+};
 
-  exports.getEditLecture = async (req, res, next) => 
-  {
-    const lectureId = req.params.lectureId;
-    const data = await Lecture.findByPk(lectureId);
-    const lecture = data.toJSON();
-    res.render("./add-lecture", {
-      pageTitle: "Edit Lecture",
-      lecture: lecture,
-      editing: true
-    });
-  }
+exports.getEditLecture = async (req, res, next) => {
+  const lectureId = req.params.lectureId;
+  const data = await Lecture.findByPk(lectureId);
+  const lecture = data.toJSON();
+  res.render("./add-lecture", {
+    pageTitle: "Edit Lecture",
+    lecture: lecture,
+    editing: true
+  });
+}
 
-  exports.postAddLecture = async (req, res, next) => {
-    const courseId = req.body.courseId;
-    const lectureId = req.body.lectureId;
-    console.log("Course ID: " + courseId);
-    if (lectureId) { 
-      await Lecture.update(req.body, { where: { id: lectureId } });
-    } else { 
-      const lecture = {
-        name: req.body.name,
-        date: req.body.date,
-        courseId: courseId
-      };
-      await Lecture.create(lecture);
-
-    }
-    res.redirect('/lectures/' + courseId);
-  };
-  
-  exports.getLectures = async (req, res, next) => {
-    const courseId = req.params.courseId;
-    const course = await Course.findOne({where: {id: courseId} });
-    const lectures = await Lecture.findAll({ raw: true, where: {courseId: courseId} });
-    res.render('./lectures', {
-      pageTitle: "Lectures",
-      lectures: lectures,
-      name: course.name,
+exports.postAddLecture = async (req, res, next) => {
+  const courseId = req.body.courseId;
+  const lectureId = req.body.lectureId;
+  console.log("Course ID: " + courseId);
+  if (lectureId) {
+    await Lecture.update(req.body, { where: { id: lectureId } });
+  } else {
+    const lecture = {
+      name: req.body.name,
+      date: req.body.date,
       courseId: courseId
-    });
-  }
+    };
+    await Lecture.create(lecture);
 
-  
+  }
+  res.redirect('/lectures/' + courseId);
+};
+
+exports.getLectures = async (req, res, next) => {
+  const courseId = req.params.courseId;
+  const course = await Course.findOne({ where: { id: courseId } });
+  const lectures = await Lecture.findAll({ raw: true, where: { courseId: courseId } });
+  res.render('./lectures', {
+    pageTitle: "Lectures",
+    lectures: lectures,
+    name: course.name,
+    courseId: courseId
+  });
+}
+
+
 exports.postDeleteLecture = async (req, res, next) => {
   const lectureId = req.body.id;
   const data = await Lecture.findByPk(lectureId);
@@ -60,3 +59,24 @@ exports.postDeleteLecture = async (req, res, next) => {
   data.destroy();
   res.redirect('/lectures/' + courseId);
 };
+
+// const multer = require('multer');
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads/');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, file.originalname);
+//   },
+// });
+
+// const upload = multer({ storage: storage });
+
+// exports.uploadLecture = upload.single('file');
+
+// exports.postUploadLecture = (req, res) => {
+//   if (!req.file) {
+//     return res.status(400).send('No file uploaded.');
+//   }
+//   res.send(`File uploaded successfully: ${req.file.originalname}`);
+// };
--- routes/lectureRoute.js
+++ routes/lectureRoute.js
@@ -1,6 +1,7 @@
 const path = require('path');
-
 const express = require('express');
+const multer = require('multer');
+const upload = multer({ dest: './uploads/' });
 
 const courseController = require('../controllers/courseController');
 const lectureController = require('../controllers/lectureController');
@@ -15,6 +16,14 @@ router.post('/edit-lecture', lectureController.postAddLecture);
 
 router.post('/delete-lecture', lectureController.postDeleteLecture);
 router.get('/lectures/:courseId', lectureController.getLectures);
+
+router.post('/upload', upload.single('lecture'), (req, res) => {
+    // req.file contains the uploaded file
+    // req.body contains the entire request body
+    const fileName = req.file.originalname;
+    // Save the file name to your database or a variable
+    res.send(`File uploaded successfully! ${fileName}`);
+  });
 
 
 
