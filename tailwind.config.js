df37107e4330956233f580e37d64a985a8244eef
dmaspoch <dmaspoch@hotmail.com>
2024-08-13 18:46:01 -0400

    Added @layer components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{html,js,hbs}"],
+  content: [
+    "./views/**/*.{html,js,hbs}",
+    "./node_modules/flowbite/**/*.js"
+  ],
   theme: {
     extend: {},
   },
   plugins: [
-    require("@tailwindcss/forms")
+    require("flowbite/plugin")
   ],
 }
 
